f5df19a087148d52f8b1a7f65c4cb680693ce4df
Ablaev Edem <141496283+6dem@users.noreply.github.com>
2024-03-15 17:31:18 +0300

    Mobile adaptation
    +form input upgrade
    +menu upgrade
    +sidebar

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const lightTheme = document.querySelector(".light-mode");
 const darkTheme = document.querySelector(".dark-mode");
 const lightPhoto = document.querySelector(".light-photo");
 const darkPhoto = document.querySelector(".dark-photo");
+const favicon = document.querySelector("#favicon");
 
 themeToggle.addEventListener("change", function() {
     if (this.checked) {
@@ -11,55 +12,29 @@ themeToggle.addEventListener("change", function() {
         darkTheme.style.display = "block"; // Показываем темный div
         lightPhoto.style.display = "none"; // Скрываем светлый div
         darkPhoto.style.display = "block"; // Показываем темный div
+        favicon.href = "img/logo_white.png";
     } else {
         document.body.classList.remove("dark-theme");
         lightTheme.style.display = "block"; // Показываем светлый div
         darkTheme.style.display = "none"; // Скрываем темный div
         lightPhoto.style.display = "block"; // Показываем светлый div
         darkPhoto.style.display = "none"; // Скрываем темный div
+        favicon.href = "img/logo_neg.png";
     }
 });
 
+const Toggle = document.getElementById('toggle-wrapper');
+const themeToggleContainer = document.getElementById('sidebar');
 
+// Функция для перемещения переключателя в соответствующий контейнер
+function moveToggle() {
+    if (window.innerWidth <= 768) {
+        themeToggleContainer.appendChild(Toggle); // Перемещаем переключатель в контейнер для маленьких устройств
+    } else {
+        document.getElementById('body').appendChild(Toggle); // Перемещаем переключатель в основной контейнер
+    }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-// // Функция для обновления переменных CSS
-// function updateThemeColors(theme) {
-// 	if (theme === "light") {
-// 			document.documentElement.style.setProperty("--bg-color", "#f5f5dc");
-// 			document.documentElement.style.setProperty("--acc-color", "#8e6244");
-// 			document.documentElement.style.setProperty("--ex-color", "#5c3c37");
-// 			document.documentElement.style.setProperty("--form-color", "#8e6244");
-// 			document.documentElement.style.setProperty("--form-text", "#fff");
-// 	} else if (theme === "dark") {
-// 			document.documentElement.style.setProperty("--bg-color", "#252850");
-// 			document.documentElement.style.setProperty("--acc-color", "#f5f5dc");
-// 			document.documentElement.style.setProperty("--ex-color", "#c9c9ac");
-// 			document.documentElement.style.setProperty("--form-color", "#f5f5dc");
-// 			document.documentElement.style.setProperty("--form-text", "#000");
-// 	}
-// }
-
-// // Функция, которая будет вызвана при изменении предпочтительной цветовой схемы
-// function handleColorSchemeChange(event) {
-// 	const preferredTheme = event.matches ? "dark" : "light";
-// 	updateThemeColors(preferredTheme);
-// }
-
-// // Добавляем обработчик события на изменение предпочтительной цветовой схемы
-// const colorSchemeQuery = window.matchMedia("(prefers-color-scheme: dark)");
-// colorSchemeQuery.addEventListener("change", handleColorSchemeChange);
-
-// // Вызываем функцию обновления цветов сразу после загрузки страницы
-// handleColorSchemeChange(colorSchemeQuery);
+// Вызываем функцию при загрузке страницы и изменении размера окна
+window.addEventListener('load', moveToggle);
+window.addEventListener('resize', moveToggle);
